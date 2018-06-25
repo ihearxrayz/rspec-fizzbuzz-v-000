f349d8ebe58951a078bb83e50a1e81655f5ef344
Louis Maldonado <louis.maldm@gmail.com>
2018-06-25 03:58:45 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,9 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz
-  
+  if int % 3 == 0
+    puts "Fizz" 
+  elsif int % 5 == 0
+    puts "Buzz"
+  else 
 end
